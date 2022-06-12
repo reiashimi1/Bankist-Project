7947ddfe46a296fab5bc247a7f538a9d8cb9f309
shime321 <reiashimi1@gmail.com>
2022-06-12 20:31:36 +0200

    Changes

--- script.js
+++ script.js
@@ -1,78 +1,38 @@
 'use strict';
 
 /////////////////////////////////////////////////
-/////////////////////////////////////////////////
 // BANKIST APP
 
 /////////////////////////////////////////////////
 // Data
-
 // DIFFERENT DATA! Contains movement dates, currency and locale
 
-const account1 = {
-  owner: 'Rei Ashimi',
-  movements: [200, 455.23, -306.5, 25000, -642.21, -133.9, 79.97, 1300],
-  interestRate: 1.2, // %
-  pin: 1111,
+let account1;
+let account2;
+let account3;
+let accounts = [];
 
-  movementsDates: [
-    '2019-11-18T21:31:17.178Z',
-    '2019-12-23T07:42:02.383Z',
-    '2020-01-28T09:15:04.904Z',
-    '2020-04-01T10:17:24.185Z',
-    '2020-05-08T14:11:59.604Z',
-    '2020-07-26T17:01:17.194Z',
-    '2020-07-28T23:36:17.929Z',
-    '2020-08-01T10:51:36.790Z',
-  ],
-  currency: 'EUR',
-  locale: 'pt-PT', // de-DE
-};
+window.addEventListener('load', fetchData, false);
 
-const account2 = {
-  owner: 'Sexhi Picaku',
-  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
-  interestRate: 1.5,
-  pin: 2222,
+function fetchData() {
+  fetch('./data.json')
+    .then(response => {
+      return response.json();
+    })
+    .then(data => {
+      accounts.push(data.accounts[0]);
+      accounts.push(data.accounts[1]);
+      accounts.push(data.accounts[2]);
 
-  movementsDates: [
-    '2019-11-01T13:15:33.035Z',
-    '2019-11-30T09:48:16.867Z',
-    '2019-12-25T06:04:23.907Z',
-    '2020-01-25T14:18:46.235Z',
-    '2020-02-05T16:33:06.386Z',
-    '2020-04-10T14:43:26.374Z',
-    '2020-06-25T18:49:59.371Z',
-    '2020-07-26T12:01:20.894Z',
-  ],
-  currency: 'USD',
-  locale: 'en-US',
-};
-
-const account3 = {
-  owner: 'Fiona Teli',
-  movements: [150, -425.73, 555.5, 20000, -642.21, -133.9, 79.97, 1300],
-  interestRate: 1.1, // %
-  pin: 3333,
-
-  movementsDates: [
-    '2019-11-28T21:31:17.178Z',
-    '2019-12-13T07:42:02.383Z',
-    '2020-01-18T09:15:04.904Z',
-    '2020-04-01T10:17:24.185Z',
-    '2020-05-08T14:11:59.604Z',
-    '2020-07-26T17:01:17.194Z',
-    '2020-07-28T23:36:17.929Z',
-    '2020-08-01T10:51:36.790Z',
-  ],
-  currency: 'EUR',
-  locale: 'en-US', // de-DE
-};
-
-const accounts = [account1, account2, account3];
+      createUsernames(accounts2);
+    })
+    .catch(err => {
+      console.log('Error');
+    });
+}
 
 /////////////////////////////////////////////////
-// Elements
+// Getting DOM Elements
 const labelWelcome = document.querySelector('.welcome');
 const labelDate = document.querySelector('.date');
 const labelBalance = document.querySelector('.balance__value');
@@ -106,16 +66,11 @@ const formatMovementDate = function (date, locale) {
     Math.round(Math.abs(date2 - date1) / (1000 * 60 * 60 * 24));
 
   const daysPassed = calcDaysPassed(new Date(), date);
-  console.log(daysPassed);
 
   if (daysPassed === 0) return 'Today';
   if (daysPassed === 1) return 'Yesterday';
   if (daysPassed <= 7) return `${daysPassed} days ago`;
 
-  // const day = `${date.getDate()}`.padStart(2, 0);
-  // const month = `${date.getMonth() + 1}`.padStart(2, 0);
-  // const year = date.getFullYear();
-  // return `${day}/${month}/${year}`;
   return new Intl.DateTimeFormat(locale).format(date);
 };
 
@@ -175,7 +130,6 @@ const calcDisplaySummary = function (acc) {
     .filter(mov => mov > 0)
     .map(deposit => (deposit * acc.interestRate) / 100)
     .filter((int, i, arr) => {
-      // console.log(arr);
       return int >= 1;
     })
     .reduce((acc, int) => acc + int, 0);
@@ -223,7 +177,7 @@ const startLogOutTimer = function () {
     time--;
   };
 
-  // Set time to 5 minutes
+  // Set time to 2 minutes
   let time = 120;
 
   // Call the timer every second
@@ -236,11 +190,6 @@ const startLogOutTimer = function () {
 ///////////////////////////////////////
 // Event handlers
 let currentAccount, timer;
-
-// FAKE ALWAYS LOGGED IN
-// currentAccount = account1;
-// updateUI(currentAccount);
-// containerApp.style.opacity = 100;
 
 btnLogin.addEventListener('click', function (e) {
   // Prevent form from submitting
@@ -266,22 +215,12 @@ btnLogin.addEventListener('click', function (e) {
       day: 'numeric',
       month: 'numeric',
       year: 'numeric',
-      // weekday: 'long',
     };
-    // const locale = navigator.language;
-    // console.log(locale);
 
     labelDate.textContent = new Intl.DateTimeFormat(
       currentAccount.locale,
       options
     ).format(now);
-
-    // const day = `${now.getDate()}`.padStart(2, 0);
-    // const month = `${now.getMonth() + 1}`.padStart(2, 0);
-    // const year = now.getFullYear();
-    // const hour = `${now.getHours()}`.padStart(2, 0);
-    // const min = `${now.getMinutes()}`.padStart(2, 0);
-    // labelDate.textContent = `${day}/${month}/${year}, ${hour}:${min}`;
 
     // Clear input fields
     inputLoginUsername.value = inputLoginPin.value = '';
@@ -324,6 +263,8 @@ btnTransfer.addEventListener('click', function (e) {
     // Reset timer
     clearInterval(timer);
     timer = startLogOutTimer();
+
+    window.alert('Money is transferred...');
   }
 });
 
@@ -361,8 +302,6 @@ btnClose.addEventListener('click', function (e) {
     const index = accounts.findIndex(
       acc => acc.username === currentAccount.username
     );
-    console.log(index);
-    // .indexOf(23)
 
     // Delete account
     accounts.splice(index, 1);
@@ -374,6 +313,7 @@ btnClose.addEventListener('click', function (e) {
   inputCloseUsername.value = inputClosePin.value = '';
 });
 
+//Sorting the data
 let sorted = false;
 btnSort.addEventListener('click', function (e) {
   e.preventDefault();
